faaf449ab2749f649af6e24f4405733d9e066015
p-laranjinha <plcasimiro@ua.pt>
2022-04-26 18:39:24 +0100

    uc2 now uses 6 partitions + changed order condition

--- PA2-G29/src/uc2/PConsumer.java
+++ PA2-G29/src/uc2/PConsumer.java
@@ -27,7 +27,7 @@ public class PConsumer {
         for(int i=0;i<6;i++){
             //don't share conditions object
             List<ConsumerDataCondition<Integer,Double>> conditions = new ArrayList<>();
-            conditions.add(new OrderedDataCondition((previous,current)-> (int) (current.timestamp()-previous.timestamp()),"Order by timestamp ascending"));
+            conditions.add(new OrderedDataCondition((previous,current)-> !(current.key().equals(previous.key()))?1:(int) (current.timestamp() - previous.timestamp()),"Same Sensor order by timestamp ascending"));
             conditions.add(new OrderedDataCondition((previous,current)-> (current.key().equals(previous.key())?0:-1 ),"Unique Sensor ID"));
             GUI gui = new GUI("Consumer " + (i+1));
             receivers[i] = new CustomKafkaConsumer(topic,props,conditions,gui);
--- PA2-G29/src/uc2/setup.sh
+++ PA2-G29/src/uc2/setup.sh
@@ -5,5 +5,5 @@ echo "Instancing Zookeeper"
 nohup ./bin/kafka-server-start.sh config/server.properties &
 echo "Instancing Kafka"
 sleep 10
-./bin/kafka-topics.sh --create --topic Sensor --bootstrap-server localhost:9092
+./bin/kafka-topics.sh --create --topic Sensor --bootstrap-server localhost:9092 --partitions 6
 echo "Setup complete"
