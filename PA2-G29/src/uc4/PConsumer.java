ab8cf360603d3c4442267c1e2b29c7995cde5dfa
buckaroo69 <diogobento@ua.pt>
2022-04-27 13:11:24 +0100

    UC 4

---
+++ PA2-G29/src/uc4/PConsumer.java
@@ -0,0 +1,45 @@
+package uc4;
+
+import util.ConsumerDataCondition;
+import util.CustomKafkaConsumer;
+import util.GUI;
+import util.OrderedDataCondition;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * An implementation of Kafka consumer ensemble<br>
+ * Deploys 6 consumer threads that attempt to stick to different partitions
+ */
+public class PConsumer {
+    public static void main(String[] args) throws InterruptedException {
+        GUI.setGUILook(new String[] { "GTK+", "Nimbus" });
+        String topic = "Sensor";
+        Properties props = new Properties();
+        props.put("bootstrap.servers","localhost:9092");
+        props.put("key.deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put("value.deserializer","org.apache.kafka.common.serialization.DoubleDeserializer");
+        props.put("group.id","0");//only 1 group for all receivers
+        props.put("partition.assignment.strategy","org.apache.kafka.clients.consumer.RoundRobinAssignor"); //1 partition per consumer hopefully
+        //TODO: MISSING PROPERTIES
+
+        CustomKafkaConsumer[] receivers = new CustomKafkaConsumer[6];
+
+        for(int i=0;i<6;i++){
+            //don't share conditions object
+            List<ConsumerDataCondition<Integer,Double>> conditions = new ArrayList<>();
+            conditions.add(new OrderedDataCondition((previous,current)-> (int) (current.timestamp() - previous.timestamp()),"Order by timestamp ascending"));
+            GUI gui = new GUI("Consumer " + (i+1));
+            receivers[i] = new CustomKafkaConsumer(topic,props,conditions,gui);
+            gui.start();
+        }
+        for(int i=0;i<6;i++)
+            receivers[i].start();
+
+        for(int i=0;i<6;i++)
+            receivers[i].join();
+
+    }
+}
---
+++ PA2-G29/src/uc4/PProducer.java
@@ -0,0 +1,47 @@
+package uc4;
+
+
+import util.CustomKafkaProducer;
+import util.GUI;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+
+/**
+ * An implementation of Kafka consumer ensemble<br>
+ * Deploys 6 producer instances with a focus on ordering and avoiding data loss<br>
+ * Each producer writers to a different partition, ordering is intra partition rather than global
+ */
+public class PProducer {
+    public static void main(String[] args) {
+        GUI.setGUILook(new String[] { "GTK+", "Nimbus" });
+        ServerSocket serverSocket;
+        int port = 8000;
+        String topic = "Sensor";
+        Properties props = new Properties();
+        props.put("bootstrap.servers","localhost:9092");
+        props.put("key.serializer","org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put("value.serializer","org.apache.kafka.common.serialization.DoubleSerializer");
+        props.put("acks","all"); //maximum ACKs to ensure linearity
+        props.put("max.in.flight.requests.per.connection","1"); //force 1 concurrent request at most for strict order
+        props.put("linger.ms","0");//default
+        props.put("compression.type","none");//default
+        props.put("batch.size","16384");//default
+        //TODO: SET EXTRA PROPERTIES HERE
+        try {
+            serverSocket = new ServerSocket(port);
+            int i = 0;
+            while (true) {
+                GUI gui = new GUI("Producer " + (i+1));
+                new CustomKafkaProducer(serverSocket.accept(),topic,props,gui,i).start();
+                gui.start();
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
